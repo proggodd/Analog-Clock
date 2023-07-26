021941594d95937ef0ceb578925449f960a07d07
ibrahim khaled <41508525+proggodd@users.noreply.github.com>
2023-07-26 06:22:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(setClock,1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+    const currentDate = new Date();
+    const secondRatio = currentDate.getSeconds()/60
+    const minuteRatio = (secondRatio+currentDate.getMinutes())/60
+    const hourRatio = (minuteRatio+currentDate.getHours())/12
+
+    setRotation(hourHand,hourRatio)
+    setRotation(minuteHand,minuteRatio)
+    setRotation(secondHand,secondRatio)
+
+}
+function setRotation(element, rotationRatio ){
+    element.style.setProperty('--rotation',rotationRatio*360)
+}
+setClock()
